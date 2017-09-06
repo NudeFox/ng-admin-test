88a08af0894a71fb2c0191eac32402a582d352d6
Drakon <gendalfwar@gmail.com>
2017-09-06 18:43:18 +0300

    Footer directive replace

--- app/footer/footer.directive.js
+++ app/footer/footer.directive.js
@@ -3,6 +3,7 @@ import footerTemplate from './footer.html';
 export default function pgFooter() {
     return {
         restrict: 'E',
+        replace: true,
         template: footerTemplate
     }
 }
